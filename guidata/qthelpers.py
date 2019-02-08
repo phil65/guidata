81d963f0a35b4f22c20845e64ec236dd6ff30a50
phil65 <phil65@kodi.tv>
2019-02-08 02:54:04 +0100

    cleanup

--- guidata/qthelpers.py
+++ guidata/qthelpers.py
@@ -13,26 +13,19 @@ The ``guidata.qthelpers`` module provides helper functions for developing
 easily Qt-based graphical user interfaces.
 """
 
-from qtpy.QtGui import (QColor, QIcon, QKeySequence)
-from qtpy.QtCore import Qt
-from qtpy.QtWidgets import (QToolButton, QMenu, QPushButton, QStyle,
-                            QWidget, QGroupBox, QAction)
-
-# Local imports:
-from guidata.py3compat import is_text_string
+from qtpy import QtGui, QtWidgets
 
 
 def text_to_qcolor(text):
     """Create a QColor from specified string"""
-    color = QColor()
-    if not is_text_string(text):  # testing for QString (PyQt API#1)
-        text = str(text)
+    color = QtGui.QColor()
+    text = str(text)
     if text.startswith('#') and len(text) == 7:
         correct = '#0123456789abcdef'
         for char in text:
             if char.lower() not in correct:
                 return color
-    elif text not in list(QColor.colorNames()):
+    elif text not in list(QtGui.QColor.colorNames()):
         return color
     color.setNamedColor(text)
     return color
@@ -45,8 +38,8 @@ def get_std_icon(name, size=None):
     """
     if not name.startswith('SP_'):
         name = 'SP_' + name
-    icon = QWidget().style().standardIcon(getattr(QStyle, name))
+    icon = QtWidgets.QWidget().style().standardIcon(getattr(QtWidgets.QStyle, name))
     if size is None:
         return icon
     else:
-        return QIcon(icon.pixmap(size, size))
+        return QtGui.QIcon(icon.pixmap(size, size))
